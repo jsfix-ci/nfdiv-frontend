edb28331f1be7b3174ecff9e786467790aa9b51b
aramchurn21 <Adesh.Ramchurn@justice.gov.uk>
2022-06-07 15:29:19 +0100

    adding welsh translation to confirm your joint application page

--- src/main/steps/applicant1/confirm-your-joint-application/content.ts
+++ src/main/steps/applicant1/confirm-your-joint-application/content.ts
@@ -1,4 +1,5 @@
 import config from 'config';
+import dayjs from 'dayjs';
 
 import { getFormattedDate } from '../../../app/case/answers/formatDate';
 import { CaseWithId, Checkbox } from '../../../app/case/case';
@@ -27,8 +28,12 @@ const en = ({ isDivorce, partner, userCase, isApplicant2, isJointApplication }: 
   } are applying to the court for ${
     isDivorce ? 'a final order of divorce' : 'the dissolution of their civil partnership'
   }`,
+  financialOrderApplicant1: 'Applicant 1 is also applying to the court to make a financial order.',
+  financialOrderApplicant2: 'Applicant 2 is also applying to the court to make a financial order.',
   caseReferenceHeading: 'Case reference number',
   caseReferenceValue: formattedCaseId(userCase.id),
+  issuedDateHeading: 'Issued',
+  issuedDateValue: dayjs(userCase.issueDate).format('D MMMM YYYY'),
   line4: 'Applicant 1',
   line5: getName(userCase, 'applicant1'),
   line6: 'Applicant 2',
@@ -42,6 +47,8 @@ const en = ({ isDivorce, partner, userCase, isApplicant2, isJointApplication }: 
       (as shown on the ${isDivorce ? 'marriage' : 'civil partnership'} certificate)`,
   line11: `Where the ${isDivorce ? 'marriage' : 'civil partnership'} took place`,
   line12: userCase.ceremonyPlace,
+  countryOfCeremonyHeading: `Country of ${isDivorce ? 'marriage' : 'civil partnership'}`,
+  countryOfCeremonyValue: userCase.ceremonyCountry,
   line13: `Date of ${isDivorce ? 'marriage' : 'civil partnership'}`,
   line14: getFormattedDate(userCase.relationshipDate),
   subHeading3: 'Why the court can deal with the case (jurisdiction)',
@@ -183,7 +190,179 @@ const en = ({ isDivorce, partner, userCase, isApplicant2, isJointApplication }: 
 });
 
 // @TODO translations
-const cy: typeof en = en;
+const cy: typeof en = ({ isDivorce, partner, userCase, isApplicant2, isJointApplication }: CommonContent) => ({
+  title: 'Cadarnhau eich cais ar y cyd',
+  subHeader: `Dyma’r wybodaeth rydych chi a’ch ${partner} wedi’i darparu ar gyfer eich cais ar y cyd. Cadarnhewch yr wybodaeth cyn parhau.`,
+  subHeading1: `${isDivorce ? 'Cais ar y cyd am ysgariad' : 'Cais ar y cyd i ddod â phartneriaeth sifil i ben'}`,
+  line1: `Dyma’r wybodaeth rydych chi a’ch ${partner} wedi’i darparu ar gyfer eich cais ar y cyd. Cadarnhewch yr wybodaeth cyn parhau.`,
+  line2: `Mae ${userCase.applicant1FirstNames} ${userCase.applicant1MiddleNames} ${userCase.applicant1LastNames} a ${
+    userCase.applicant2FirstNames
+  } ${userCase.applicant2MiddleNames} ${userCase.applicant2LastNames} yn gwneud cais i’r llys ${
+    isDivorce ? 'am orchymyn ysgaru terfynol.' : 'i ddiddymu eu partneriaeth sifil.'
+  } ${userCase.applicant1ApplyForFinancialOrder}`,
+  financialOrderApplicant1: 'Mae Ceisydd 1 hefyd yn gwneud cais i’r llys i wneud gorchynyn ariannol.',
+  financialOrderApplicant2: 'Mae Ceisydd 2 hefyd yn gwneud cais i’r llys i wneud gorchynyn ariannol.',
+  caseReferenceHeading: 'Cyfeirnod yr achos',
+  caseReferenceValue: formattedCaseId(userCase.id),
+  issuedDateHeading: 'Dyddiad cyflwyno',
+  issuedDateValue: dayjs(userCase.issueDate).format('D MMMM YYYY'),
+  line4: 'Ceisydd 1',
+  line5: getName(userCase, 'applicant1'),
+  line6: 'Ceisydd 2',
+  line7: getName(userCase, 'applicant2'),
+  subHeading2: `Ynghylch y ${isDivorce ? 'briodas' : 'bartneriaeth sifil'}`,
+  line8: `Mae’r manylion hyn wedi’u copïo’n uniongyrchol oddi ar y dystysgrif ${
+    isDivorce ? 'priodas' : 'partneriaeth sifil'
+  },
+     neu gyfieithiad o’r dystysgrif os nad yw’n Saesneg. Yr enwau ar y dystysgrif yw’r enwau roedd y ceiswyr yn eu defnyddio cyn y  ${
+       isDivorce ? 'briodas' : 'bartneriaeth sifil'
+     }.`,
+  line9: `Rhwng pwy y mae’r ${isDivorce ? 'briodas' : 'bartneriaeth sifil'}`,
+  line10: `${userCase.applicant1FullNameOnCertificate} a ${userCase.applicant2FullNameOnCertificate}
+      (fel y dangosir ar y dystysgrif ${isDivorce ? 'priodas' : 'partneriaeth sifil'})`,
+  line11: `Lle digwyddodd y ${isDivorce ? 'briodas' : 'bartneriaeth sifil'}`,
+  line12: userCase.ceremonyPlace,
+  countryOfCeremonyHeading: `Gwlad y ${isDivorce ? 'briodas' : 'bartneriaeth sifil'}`,
+  countryOfCeremonyValue: userCase.ceremonyCountry,
+  line13: `Dyddiad y ${isDivorce ? 'briodas' : 'bartneriaeth sifil'}`,
+  line14: getFormattedDate(userCase.relationshipDate),
+  subHeading3: 'Pam gall y llys ddelio â’r achos (awdurdodaeth)',
+  line15: 'Mae gan llysoedd Cymru a Lloegr bŵer cyfreithiol (awdurdodaeth) i ddelio â’r achos hwn oherwydd:',
+  connectionBulletPoints:
+    userCase && userCase.connections
+      ? enConnectionBulletPointsSummarisedForAllUsers(userCase.connections, isDivorce, isJointApplication)
+      : [],
+  jurisdictionsMoreDetails: {
+    part1: `The courts of England or Wales must have the legal power (jurisdiction) to be able to ${
+      isDivorce ? 'grant a divorce' : 'end a civil partnership'
+    }.
+    The applicants confirmed that the legal statement(s) in the application apply to either or both the applicants.
+     Each legal statement includes some or all of the following legal connections to England or Wales.`,
+    part2: jurisdictionMoreDetailsContent(userCase.connections, isDivorce, true).text,
+  },
+  whatThisMeans: 'What this means',
+  subHeading4: 'Other court cases',
+  line16: `The court needs to know about any other court cases relating to the ${
+    isDivorce ? 'marriage' : 'civil partnership'
+  }, which might affect the legal power (jurisdiction) of the court.`,
+  line17: `${
+    userCase.applicant1LegalProceedings === YesOrNo.YES && userCase.applicant2LegalProceedings === YesOrNo.YES
+      ? `Applicant 1 has given details of other court cases relating to the ${
+          isDivorce ? 'marriage' : 'civil partnership'
+        }:` +
+        '<br>' +
+        userCase.applicant1LegalProceedingsDetails +
+        '<br><br>' +
+        `Applicant 2 has given details of other court cases relating to the ${
+          isDivorce ? 'marriage' : 'civil partnership'
+        }:` +
+        '<br>' +
+        userCase.applicant2LegalProceedingsDetails
+      : userCase.applicant1LegalProceedings === YesOrNo.YES
+      ? `Applicant 1 has given details of other court cases relating to the ${
+          isDivorce ? 'marriage' : 'civil partnership'
+        }:` +
+        '<br>' +
+        userCase.applicant1LegalProceedingsDetails
+      : userCase.applicant2LegalProceedings === YesOrNo.YES
+      ? `Applicant 2 has given details of other court cases relating to the ${
+          isDivorce ? 'marriage' : 'civil partnership'
+        }:` +
+        '<br>' +
+        userCase.applicant2LegalProceedingsDetails
+      : `The applicants have indicated that there are no other court cases which are related to the ${
+          isDivorce ? 'marriage' : 'civil partnership'
+        }`
+  }.`,
+  subHeading5: `Reason for  ${isDivorce ? 'the divorce' : 'ending the civil partnership'}`,
+  line18: `The ${isDivorce ? 'marriage' : 'relationship'} has broken down irretrievably (it cannot be saved).`,
+  subHeading6: 'Financial order application',
+  applicant1FinancialOrderYes: `Applicant 1 is applying to the court for financial orders for ${userCase.applicant1WhoIsFinancialOrderFor
+    ?.sort()
+    .join(' and ')
+    .replace(FinancialOrderFor.APPLICANT, 'themselves')
+    .replace(FinancialOrderFor.CHILDREN, 'the children')}.`,
+  applicant2FinancialOrderYes: `Applicant 2 is applying to the court for financial orders for ${userCase.applicant2WhoIsFinancialOrderFor
+    ?.sort()
+    .join(' and ')
+    .replace(FinancialOrderFor.APPLICANT, 'themselves')
+    .replace(FinancialOrderFor.CHILDREN, 'the children')}.`,
+  financialOrderNo: 'The applicants are not applying to the court for financial orders.',
+  financialOrderMoreDetails: `You and your ${partner} were asked if you want the court to decide how your money, property,
+ pensions and other assets will be split. These decisions are called ‘financial orders’.
+  <br><br>A financial order can be made if you agree about dividing money and property, and you want to make the decision legally binding.
+   This is known as a ‘financial order by consent’. Or they can be made if you disagree about dividing money and property and want the court to decide.
+    This is known as a ‘contested financial order’.
+  <br><br>To formally start legal proceedings, the applicants will need to complete another form and pay a fee.
+   Applying for a ‘contested financial order’ costs ${getFee(
+     config.get('fees.financialOrder')
+   )}. Applying for a ‘financial order by consent’ costs ${getFee(
+    config.get('fees.consentOrder')
+  )}. You can get a solicitor to draft these for you.
+  <br><br>If you are not sure what to do then you should seek legal advice. `,
+  subHeading7: "Applicant 1's correspondence address",
+  applicantAddressCountry: `${
+    userCase.applicant1SolicitorAddress ||
+    [
+      userCase.applicant1Address1,
+      userCase.applicant1Address2,
+      userCase.applicant1Address3,
+      userCase.applicant1AddressTown,
+      userCase.applicant1AddressCounty,
+      userCase.applicant1AddressPostcode,
+      userCase.applicant1AddressCountry,
+    ]
+      .filter(Boolean)
+      .join('<br>')
+  }`,
+  subHeading8: "Applicant 1's email address",
+  line19: `${userCase.applicant1Email}`,
+  subHeading9: "Applicant 2's postal address",
+  respondentAddressCountry: `${
+    userCase.applicant2SolicitorAddress ||
+    [
+      userCase.applicant2Address1,
+      userCase.applicant2Address2,
+      userCase.applicant2Address3,
+      userCase.applicant2AddressTown,
+      userCase.applicant2AddressCounty,
+      userCase.applicant2AddressPostcode,
+      userCase.applicant2AddressCountry,
+    ]
+      .filter(Boolean)
+      .join('<br>')
+  }`,
+  subHeading10: "Applicant 2's email address",
+  line20: `${userCase.applicant2Email}`,
+  confirm: `Confirm before  ${isSubmit(isApplicant2, userCase) ? 'submitting' : 'continuing'}`,
+  confirmPrayer: `I confirm that I’m applying to the court to ${
+    isDivorce ? 'dissolve my marriage (get a divorce)' : 'end my civil partnership'
+  } ${
+    userCase.applicant1ApplyForFinancialOrder === YesOrNo.YES ||
+    userCase.applicant2ApplyForFinancialOrder === YesOrNo.YES
+      ? 'and make financial orders to decide how our money and property will be split.'
+      : ''
+  }`,
+  confirmPrayerHint: 'This confirms what you are asking the court to do. It’s known as ‘the prayer’.',
+  confirmApplicationIsTrue: 'I believe that the facts stated in this application are true',
+  confirmApplicationIsTrueHint:
+    'This confirms that the information you are submitting is true and accurate to the best of your knowledge. ' +
+    'It’s known as the ‘statement of truth’.',
+  continue: `${isSubmit(isApplicant2, userCase) ? 'Submit' : 'Continue to payment'}`,
+  confirmApplicationIsTrueWarning:
+    'Proceedings for contempt of court may be brought against anyone who makes, or causes to be made, a false statement ' +
+    'verified by a statement of truth without an honest belief in its truth.',
+  errors: {
+    applicant1IConfirmPrayer: {
+      required: `You need to confirm you are applying to the court to  ${
+        isDivorce ? 'dissolve your marriage (get a divorce)' : 'end your civil partnership'
+      }.`,
+    },
+    applicant1StatementOfTruth: {
+      required: 'You need to confirm the facts stated in this application are true',
+    },
+  },
+});
 
 export const form: FormContent = {
   fields: {
